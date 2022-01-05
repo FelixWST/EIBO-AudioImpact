43e137490c069c678109a49b7adb54e9017eeca6
FelixWST <felix@xn--wst-hoa.net>
2022-01-05 14:00:00 +0100

    First Frame

--- src/presentation/application/Main.java
+++ src/presentation/application/Main.java
@@ -1,11 +1,23 @@
 package presentation.application;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    private Stage primaryStage;
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage){
+        this.primaryStage = primaryStage;
+        Scene scene = new Scene(new HBox(), 500, 500);
+        primaryStage.setScene(scene);
+        primaryStage.show();
     //Test Comment
+    }
+
+    public void stop(){
+        System.exit(0);
     }
 }
