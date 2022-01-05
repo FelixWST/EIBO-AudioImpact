616bcc2e48d51b194df76909e2af1bfa96772c53
FelixWST <felix@xn--wst-hoa.net>
2022-01-05 13:23:01 +0100

    Application

--- src/presentation/application/Main.java
+++ src/presentation/application/Main.java
@@ -1,4 +1,11 @@
 package presentation.application;
 
-public class Main {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
 }
