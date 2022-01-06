c5c169fcb8c25d94952270169a2aee1c6ed3b87b
FelixWST <felix@xn--wst-hoa.net>
2022-01-06 14:58:57 +0100

    AudioTestTrack included

--- src/business/tracks/AudioTrackType.java
+++ src/business/tracks/AudioTrackType.java
@@ -3,7 +3,9 @@ package business.tracks;
 import javafx.scene.paint.Color;
 
 public enum AudioTrackType {
-    INTENSITY(Color.rgb(251,60,59), "#FB3C3B"), DEPTH(Color.rgb(252,196,41),"#FCC429"), ATMOSPHERE(Color.rgb(85,254,248),"#55FEF8");
+    INTENSITY(Color.rgb(251,60,59), "#FB3C3B"),
+    DEPTH(Color.rgb(252,196,41),"#FCC429"),
+    ATMOSPHERE(Color.rgb(85,254,248),"#55FEF8");
 
     private Color colorRGB;
     private String colorHEX;
